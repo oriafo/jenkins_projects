9273a888f61b77a3a1379464de9209d0b17d3ff7
oriafo <abrahamibharunujele@gmail.com>
2023-07-22 09:15:45 +0100

    modified myfile.py

--- image_from_jenkins/myfile.py
+++ image_from_jenkins/myfile.py
@@ -1,3 +1,3 @@
 print("Hello guys, this time i would try and run the build on jenkins")
 print("This is getting interesting")
-print("Just crea2ted the jenkins filee")
+print("Just crea2ted the jenkins file")
